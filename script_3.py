3fd723cb033bce4903718d5a1fd1aa4ff05c52e1
some-repository <patch.code2002@gmail.com>
2021-04-22 16:54:37 +0300

    changed script_3.py

--- script_3.py
+++ script_3.py
@@ -2,23 +2,32 @@ import math
 import matplotlib.pyplot as plt
 import main_script_module as main
 import time
+import RPi.GPIO as GPIO
 
 main.GPIOinit ()
 
-def sin_gen (time, frequency, samplingFrequency):
+def sin_gen (tim, frequency, samplingFrequency):
     omega = 2*(math.pi)*frequency
-    n = time*samplingFrequency
+    n = tim*samplingFrequency
     ndarray = []
     t = []
     for i in range (0, n, 1):
-        ndarray.append (int (255 * math.sin(omega*time*(i/n))))
+        ndarray.append (int (255 * math.sin(omega*tim*(i/n))))
     for i in range (0, n, 1):
         t.append (i)
     plt.plot (t, ndarray)
     plt.show ()
-    for i in range (0, n, 1):
-        main.lightNumber (ndarray [i])
-        time.sleep (0.01)
+    t = 1
+    while t == 1:
+    	try:
+    		for i in range (0, n, 1):
+        		main.lightNumber (ndarray [i])
+        		time.sleep (tim)
+    	except: KeyboardInterrupt:
+    		t = 0
+    		for i in main.LEDs:
+        		GPIO.output(i,0)
+    		GPIO.cleanup ()
 
-sin_gen (1, 10, 100)
+sin_gen (10, 1000, 100)
 
